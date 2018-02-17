c59834fe481b32195caed75ecdb7783cc1bae880
abdulrahman alaa <abdulrahman alaa>
2018-02-17 20:23:16 +0200

    finishing all the i18 english language

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { TranslateModule } from '@ngx-translate/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -12,7 +13,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule,
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    TranslateModule
   ],
   declarations: [LoginComponent]
 })
